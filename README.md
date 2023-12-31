de6f6397fa0935936323ed27efd1225cc6160bed
kongpf <kongpf8848@gmail.com>
2023-12-31 18:27:20 +0800

    migrate directory

--- README.md
+++
@@ -1,4 +0,0 @@
-# rich_editor
-rich editor base on [flutter-quill](https://github.com/singerdmx/flutter-quill) for Android, iOS platform.
-
-
--- android/app/src/main/kotlin/io/github/kongpf8848/rich_editor/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.github.kongpf8848.rich_editor
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
